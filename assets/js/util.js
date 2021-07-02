839e1bdba533a6f68bd6f02da5ac826524f1d7aa
carlos <carlos.henriquez.lara@gmail.com>
2021-07-01 21:02:00 -0400

    first commit

---
+++ assets/js/util.js
@@ -0,0 +1,52 @@
+let nombre = prompt('Ingrese su nombre');
+
+document.write('<h1>Hola ' + nombre +'</h1>');
+
+/*
+let dividendo = 15;
+let divisor = 4;
+
+let resultado = dividendo % divisor;
+
+console.log(resultado);
+
+if(dividendo !== '15') {
+    console.log('es distinto');
+} 
+else {
+    console.log('igual');
+}
+
+//let num1 = document.getElementById('numero-1').innerHTML;
+//let num2 = document.getElementById('numero-2').innerHTML;
+
+
+let num1 = 5;
+let num2 = num1 + 2;
+
+
+console.log(num1, num2);
+
+
+if(resultado <= 11) {
+    alert('resultado es igual a 11');
+}
+else {
+    alert('resultado distinto a 11');
+}
+*/
+
+/*
+let flag = true;
+
+let resultado = 'Hola mundo: el resultado de ' + 
+num1 + ' dividido en ' + 
+num2 + ' es igual a: ' + (num1 / num2);
+
+console.log(flag);
+
+//resultado = 120 * 2;
+
+document.getElementById('resultado').innerHTML = resultado;
+
+*/
